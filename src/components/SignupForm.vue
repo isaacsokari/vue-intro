7108ca20727473ab1012b03c3a0165772641afce
Isaac Sokari <isaacsokari@gmail.com>
2020-12-25 10:52:05 +0100

    added select and checkbox

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,13 +1,22 @@
 <template>
   <form>
-    <label for="email">Email</label>
+    <label for="email">Email:</label>
     <input type="email" name="email" id="email" v-model="email" />
-    <label for="password">Password</label>
+    <label for="password">Password:</label>
     <input type="password" name="password" id="password" v-model="password" />
+    <label for="role">Role:</label>
+    <select name="role" id="role" v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+    <input type="checkbox" required name="terms" id="terms" v-model="terms" />
+    <label for="terms">Accept Terms and Conditions</label>
   </form>
 
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Accepted Terms: {{ terms }}</p>
 </template>
 
 <script>
@@ -16,6 +25,8 @@ export default {
     return {
       email: '',
       password: '',
+      role: 'developer',
+      terms: false,
     };
   },
   methods: {
@@ -44,7 +55,8 @@ label {
   letter-spacing: 1px;
   font-weight: bold;
 }
-input {
+input,
+select {
   display: block;
   padding: 10px 6px;
   width: 100%;
@@ -52,5 +64,24 @@ input {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+input[type='checkbox'] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
 }
 </style>
